9eacf2cc0c073bf85eeac971b9ebfe11062e8d1f
Samer Alotaibi <sam.otb@hotmail.ca>
2025-03-27 09:44:15 +0200

    lar12

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/index.tsx'],
+            refresh: true,
+        }),
+        react()
+    ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+          host: 'localhost'
+        },
+     },
+     build: {
+        sourcemap: true,
+        manifest: true,
+        outDir: './public/build',
+        emptyOutDir: false
+     },
+     esbuild: {
+        include: /\.(tsx?|jsx?)$/,
+        exclude: [],
+        loader: 'tsx'
+      },
+});
